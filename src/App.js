108cd7ae6105436fc81832e0aa2072ca14c6f4ae
이정재 <st20916@naver.com>
2024-04-19 15:18:50 +0900

    feat : example React Project is Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";        // Route 사용하기 위한
+
+import Header from './component/layout/Header';                         // import Header 컴포넌트
+import Main from './component/Main';                                    // import Main 컴포넌트
+import Footer from './component/layout/Footer';                         // import Footer 컴포넌트
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <BrowserRouter>
+            <Header />
+            {/* Route 사용 */}
+            <Routes>
+                <Route path='/' element={<Main />}></Route>             {/* Main */}
+            </Routes>
+            <Footer />
+        </BrowserRouter>
     </div>
   );
 }
---
+++ src/component/layout/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header id='header' className='header'>
+            
+        </header>
+    );
+}
+
+export default Header;
