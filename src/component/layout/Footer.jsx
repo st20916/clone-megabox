d8401efc8c51dd383253155dd1659d1beecb4ac3
이정재 <st20916@naver.com>
2024-04-19 16:56:48 +0900

    feat : add footer css

--- src/component/layout/Footer.jsx
+++ src/component/layout/Footer.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import '../../styles/layout/footer.css';                    // Import footer css
+
 const Footer = () => {
     return (
         <footer id='footer' className='footer'>
-
+            <div className='footer_wrap'>
+                
+            </div>
         </footer>
     );
 }
