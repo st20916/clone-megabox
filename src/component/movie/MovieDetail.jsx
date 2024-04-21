6ff1d1bb4c5b4fd37e626c0e19584e33c1e02ee6
이정재 <st20916@naver.com>
2024-04-21 14:29:09 +0900

    feat : add info in detail page

--- src/component/movie/MovieDetail.jsx
+++ src/component/movie/MovieDetail.jsx
@@ -1,10 +1,60 @@
 import React from 'react';
+import { useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const MovieDetail = () => {
-    return (
-        <div>
+    const location = useLocation()
+        , searchParams = new URLSearchParams(location.search)
+        , movieId = searchParams.get('seq')
+        , movies = useSelector(state => state.movies)
+        , selectedMovie = movies.find(movie => movie.id === parseInt(movieId));
 
-        </div>
+    // inline Style
+    const inlineStyles = {
+        backgroundImage: `url(${selectedMovie.imagePath})`
+    }
+
+    if (!selectedMovie) {
+        return <div>영화를 찾을 수 없습니다.</div>;
+    }
+
+    return (
+        <main id='main'>
+            <div className='movie_detail_page'>
+                <div className='bg_img' style={inlineStyles}></div>
+                <div className='bg_mask'></div>
+                <div className='cont'>
+                    <div className='info'>
+                        <h1 className='tit'>{selectedMovie.name}</h1>
+                        <div className='btn_util'>
+                            <button type='button' className='btn_line3'>
+                                <span className='ri-heart-line'></span>
+                                1.4K
+                            </button>
+                            <button type='button' className='btn_line3'>
+                                <span className='ri-heart-line'></span>
+                                1.4K
+                            </button>
+                        </div>
+                        <div className='grade'>
+                            <strong>관람 등급</strong>
+                            <p>{selectedMovie.ageAttendance}</p>
+                        </div>
+                    </div>
+                    <div className='poster'>
+                        <img src={selectedMovie.imagePath} alt={selectedMovie.name}/>
+                    </div>
+                </div>
+            </div>
+            <div className='main_wrap'>
+                <div id='contents'>
+                    {/* movie */}
+                    <div id='contents_body'>
+
+                    </div>
+                </div>
+            </div>
+        </main>
     );
 }
 
